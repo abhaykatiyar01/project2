80161395d8e698423ffc5c1b2d5d46d757e34ebf
Abhay Katiyar <www.abhaykatiyar@gmail.com>
2025-06-05 11:25:05 +0530

    Add files via upload

--- file.js
+++ file.js
@@ -9,16 +9,16 @@
         return color;
         }
         function move(){
-            var left;
-            var top;
-            var wh;
-            left =Math.random()*300;
-            right =Math.random()*300;
-            wh = ((Math.random()*400) + 100);
-            document.getElementById("box1").style.left = left;
-            document.getElementById("box1").style.top = top;
-            document.getElementById("box1").style.width = wh;
-            document.getElementById("box1").style.height = wh;
+            //var left;
+           // var top;
+           // var wh;
+            var left =Math.random()*300;
+            var top =Math.random()*300;
+           var wh = ((Math.random()*400) + 100);
+            document.getElementById("box1").style.left = left + "px";
+            document.getElementById("box1").style.top = top + "px";
+            document.getElementById("box1").style.width = wh + "px";
+            document.getElementById("box1").style.height = wh + "px";
             document.getElementById("box1").style.display = "block";
             document.getElementById("box1").style.backgroundColor = getRandomColor();
             start = new Date().getTime();
